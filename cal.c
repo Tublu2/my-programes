ed4355cdf589b75c77d8eca74eaa75b2ac4c63a1
Anupam Roy <sampu@gmx.us>
2020-09-26 21:10:08 +0530

    Update cal.c

--- cal.c
+++ cal.c
@@ -4,10 +4,13 @@ int main()
  char operator;
  int i,j, e=0;
  printf("enter the operator (+,-,*,/,e)");
- scanf("%c",&operator);
+ operator=getchar(); // better then scanning using scanf("%c",&operator);
  printf("enter two numbers ");
  scanf("%d %d ",&i,&j);
- switch(operator)
+ 
+ while(1) {     // Infinite loop
+ 
+    switch(operator)
        {   
         case '+':
       	printf("%d + %d = %d",i,j,i+j); break;
@@ -17,7 +20,8 @@ int main()
      	printf("%d * %d = %d",i,j,i*j); break;
  	case '/':
      	printf("%d / %d = %d",i,j,i/j); break;
-        case 'e': return 0;
-       }                    
+    case 'e': return 0; //Loop breaks and program exits here
+       }  
+ }
  return 0;
 }
