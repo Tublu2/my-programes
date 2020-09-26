0e038a2a4afd6bab3dc1f9644359fb6b45d68a71
Subhajit <subhajitbardhan12@gmail.com>
2020-09-26 12:41:46 +0530

    uploding

--- cal.c
+++ cal.c
@@ -8,9 +8,8 @@ int main()
  printf("enter two numbers ");
  scanf("%d %d ",&i,&j);
  switch(operator)
-  {        
-     if( ("+,-,*,/"))      //--------------------------remove this if. not needed
-       {                   //  remove this line
+        
+       {   
         case '+':
       	printf("%d + %d = %d",i,j,i+j); break;
  	case '-':
@@ -19,13 +18,10 @@ int main()
      	printf("%d * %d = %d",i,j,i*j); break;
  	case '/':
      	printf("%d / %d = %d",i,j,i/j); break;
-                          // -----------------------putthis here-> case 'e': return 0;
-       }
+                          
+      
    
- //------------------------ remove this part of code
-//       else
-//          {
-//            printf("to exit press value = %d ",e);
-//          }
+ 
+   
 }
 }
